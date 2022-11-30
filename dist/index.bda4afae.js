fa28a812166eaa3f113bf3b76f32632829a0b7d8
MoonLGH <MoonLMC1212@gmail.com>
2022-11-30 05:32:38 +0000

    Init Absen

---
+++ dist/index.bda4afae.js
@@ -0,0 +1,13 @@
+function absen() {
+    let namaAbsen = document.getElementById("namaAbsen").value;
+    let kelas = document.getElementById("kelas").value;
+    let status = document.getElementById("status").value;
+    let alasan = document.getElementById("alasan").value;
+    let kelasStr = document.getElementById("kelasStr").value;
+    console.log(namaAbsen);
+    console.log(kelas + kelasStr);
+    console.log(status);
+    console.log(alasan);
+}
+
+//# sourceMappingURL=index.bda4afae.js.map
